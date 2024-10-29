f444145081876eccbb144adf41969183036ffeb0
Miguel de Campos Rodrigues Moret <miguel.moret@unesp.br>
2024-10-29 15:40:55 -0300

    did some stuff to be clear to understand

--- Aid/leitor.c
+++ Aid/leitor.c
@@ -4,5 +4,16 @@
 
 int main() {
 
-    
+    int n = 1000;
+    char *filename = "../Arrays/random/1000.bin";
+
+    int *array = readArrayFromFile(n, filename);
+    if (array == NULL)
+    {
+        return 1;
+    }
+
+    printArray(array, n);
+
+    return 0;
 }
